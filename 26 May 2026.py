3d27b6e719084a61381b491b0e7761690fee9e96
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-26 10:03:13 +0530

    String formating

--- 26 May 2026.py
+++ 26 May 2026.py
@@ -6,3 +6,15 @@ y=input("Enter y: ")
 print("The vlue is: ",y) #to print something with a value write like this string formating
 print("Hello \n world") #new line character
 print("I am Tushar")
+
+#STRING FORMAT
+a="Tushar"
+b="Chinmaya"
+print(f"Hello {a}")
+print("Hello {}".format(b))
+print(f"Hello {a} and {b}")
+print("Hello {0} and {1}".format(a, b))
+s=f"Hello {a}" #f string formating
+print(s)
+r="Hello {}".format(b)
+print(r)
