7c9b359f071edcf31276ad58fb202f56c6a376a9
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-27 09:15:07 +0530

    floor & ceil value

--- 27 May 2026.py
+++ 27 May 2026.py
@@ -10,3 +10,6 @@ print(x+y) #we will get the output as 2020, as these are strings
 x=int(input("Enter first number: "))
 y=int(input("Enter second number: "))
 print(x+y) #we will get the value as 40
+import math
+print(math.floor(3.5))
+print(math.ceil(3.5))
