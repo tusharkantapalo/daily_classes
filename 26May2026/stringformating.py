b5eee2f7387297d0cadc723f2b5f4963408308ad
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-28 08:56:02 +0530

    edited

--- 26May2026/stringformating.py
+++ 26May2026/stringformating.py
@@ -1,12 +1,3 @@
-x=10.5
-print(type(x))
-print(id(x))
-print(x)
-y=input("Enter y: ")
-print("The vlue is: ",y) #to print something with a value write like this string formating
-print("Hello \n world") #new line character
-print("I am Tushar")
-
 #STRING FORMAT
 a="Tushar"
 b="Chinmaya"
--- 27May2026/operator.py
+++ 27May2026/operator.py
@@ -1,8 +1,3 @@
-#TYPE CASTING
-age=input("Enter your age: ")
-print(type(age)) #Here it is a string
-age=int(input("Enter your age: "))
-print(type(age)) #Here it is an integer
 #OPERATORS
 x=input("Enter first number: ")
 y=input("Enter second number: ")
