4a91c4b2176c395ea4e7ae34f7c75d0b0d6f49cc
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-29 09:12:51 +0530

    added isalpha()

---
+++ 29May2026/isalpha().py
@@ -0,0 +1,7 @@
+exp = input("Enter the expression: ")
+if exp.isalpha():
+    print("True")
+else:
+    print("False")
+'''The string can have upper and lower case letters but can't
+have space or special characters'''
