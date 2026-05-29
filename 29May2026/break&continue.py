4776c28bc8eb52d16706c9c0d5d05bdce0f11ebb
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-29 09:30:28 +0530

    added break and continue

---
+++ 29May2026/break&continue.py
@@ -0,0 +1,8 @@
+exp = input("Enter a string: ")
+for i in range(0,len(exp),1):
+    if(((exp[i]>='a')and(exp[i]<='z'))or((exp[i]>='A')and(exp[i]<='Z'))):
+        continue #print("True")
+    else:
+        break
+else:  #else is also used with for loop
+    print("True")
