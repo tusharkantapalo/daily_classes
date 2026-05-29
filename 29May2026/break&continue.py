76d2b4f7d6606c1cd920442564a463da1ed65572
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-29 09:31:46 +0530

    edited

--- 29May2026/break&continue.py
+++ 29May2026/break&continue.py
@@ -3,6 +3,7 @@ for i in range(0,len(exp),1):
     if(((exp[i]>='a')and(exp[i]<='z'))or((exp[i]>='A')and(exp[i]<='Z'))):
         continue #print("True")
     else:
+        print("False")
         break
 else:  #else is also used with for loop
     print("True")
