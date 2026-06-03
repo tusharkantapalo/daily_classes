75e7fade1859e47d0df07e53a515ee791ded9fa1
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-03 20:41:54 +0530

    opposite printing using numbers

---
+++ 3June2026/lab/ano.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number: "))
+for i in range(1, n + 1):
+    # Print leading spaces
+    for j in range(n - i):
+        print("  ", end="")
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
