493d885848196c938479d003dbcbc6716e169b35
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-06 10:17:31 +0530

    corrected

--- 6June2026/theory/docstring.py
+++ 6June2026/theory/docstring.py
@@ -2,10 +2,13 @@
 def sum_of_two_nos(a: int, b: int) -> int:
     '''
     This function accepts two arguments and returns the sum of them
-    function:
-        a: integer
-        b: integer
-        return: integer
+    
+    arguments:
+        a (int): first number
+        b (int): second number
+    
+    returns:
+        int: sum of a and b
     '''
     return a + b
 '''Here (a: int) means a is of integer and also with b and 
