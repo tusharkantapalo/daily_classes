0ada7c553fdb1617ababc260fe63ac717cc6da17
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-08 08:47:51 +0530

    returning the two numbers from the list whose sum is equal to target

---
+++ 8June2026/theory/sumof_n0s.py
@@ -0,0 +1,21 @@
+n = int(input("Enter the number of inputs: "))
+
+l = []
+for i in range(n):
+    l.append(int(input("Enter: ")))
+
+target = int(input("Enter the target: "))
+
+l1 = []
+
+'''for i in l:
+    for j in l:
+        if i + j == target:
+            l1.append((i, j))'''
+
+for i in range(len(l)):
+    for j in range(i + 1, len(l)):
+        if l[i] + l[j] == target:
+            l1.append((l[i], l[j]))
+
+print(f"The pairs are: {l1}")
