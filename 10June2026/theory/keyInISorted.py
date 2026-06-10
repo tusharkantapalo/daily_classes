dbc3cc3fa02fbf49ad4043e3b5d6370ccf778143
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-10 08:35:24 +0530

    key in sorted statement

--- 10June2026/theory/keyInISorted.py
+++ 10June2026/theory/keyInISorted.py
@@ -1,8 +1,12 @@
 '''Write aprogram to arrange the list of tuple by its first element'''
+
 n = int(input("Enter the number of inputs: "))
+
 list = []
+
 for i in range(n):
     list.append((int(input("Enter 1st element: ")), int(input("Enter 2nd element: "))))
 
 list_sorted = sorted(list, key = lambda x: x[0], reverse = True)
+
 print(f"Sorted list is: {list_sorted}")
